1ac3a3abba8caa4df59c045b8bfec683a01238a0
Steven Lynn <i@stv.pm>
2026-09-10 16:51:02 +0800

    fix(frontend): render SEO outside dynamic theme layout

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -85,8 +85,8 @@ const MyApp = ({ Component, pageProps }: AppProps<PageProps>) => {
   const content = (
     <AppErrorBoundary>
       <GlobalContextProvider {...pageProps}>
+        <SEO {...pageProps} />
         <GLayout {...pageProps}>
-          <SEO {...pageProps} />
           <Component {...pageProps} />
         </GLayout>
         <ExternalPlugins {...pageProps} />
